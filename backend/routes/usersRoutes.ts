398b92c91f8099fdc99b3564e5633875cd108ef5
aaronb12-ux <afairster03@gmail.com>
2025-11-24 20:39:01 -0800

    register (w hashed password), getuserbyid, and login with returned json token all working

--- backend/routes/usersRoutes.ts
+++ backend/routes/usersRoutes.ts
@@ -1,9 +1,10 @@
 import { Router, type Request, type Response } from "express";
-import {createUserController, getUserByIDController} from "../controllers/usersControllers.ts";
+import {createUserController, getUserByIDController, loginUserController} from "../controllers/usersControllers.ts";
 
 const usersRouter = Router();
 
 usersRouter.post("/register", (req: Request, res: Response) => createUserController(req, res)) //create user
 usersRouter.get("/users/:id", (req: Request, res: Response) => getUserByIDController(req, res)) //get user by id
+usersRouter.post("/log-in", (req: Request, res: Response) => loginUserController(req, res))
 
 export default usersRouter;
